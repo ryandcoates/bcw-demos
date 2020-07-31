c3a2a3bb90ceb2ba9f85c35638ed322383995727
Ryan Coates <ryan.coates@gmail.com>
2020-07-31 14:17:13 -0600

    updated storage stack with demo code

--- cloud-storage/MyStack.cs
+++ cloud-storage/MyStack.cs
@@ -1,26 +1,50 @@
 using Pulumi;
 using Pulumi.Azure.Core;
 using Pulumi.Azure.Storage;
+using Pulumi.Azure.Storage.Inputs;
 
 class MyStack : Stack
 {
     public MyStack()
     {
         // Create an Azure Resource Group
-        var resourceGroup = new ResourceGroup("resourceGroup");
+        var resourceGroup = new ResourceGroup("bcw-july20-storage");
 
         // Create an Azure Storage Account
         var storageAccount = new Account("storage", new AccountArgs
         {
             ResourceGroupName = resourceGroup.Name,
             AccountReplicationType = "LRS",
-            AccountTier = "Standard"
+            AccountTier = "Standard",
+            StaticWebsite = new AccountStaticWebsiteArgs
+            {
+                IndexDocument = "index.html"
+            },
+            Tags =
+            {
+                { "Environment", "Demo" }
+            }
         });
 
-        // Export the connection string for the storage account
-        this.ConnectionString = storageAccount.PrimaryConnectionString;
+        var files = new[] { "index.html", "404.html" };
+        foreach (var file in files)
+        {
+            var uploadedFile = new Blob(file, new BlobArgs
+            {
+                Name = file,
+                StorageAccountName = storageAccount.Name,
+                StorageContainerName = "$web",
+                Type = "Block",
+                Source = new FileAsset($"./wwwroot/{file}"),
+                ContentType = "text/html",
+            });
+        }
+
+        // Export the connection string and storage account URL for the storage account
+        // this.ConnectionString = storageAccount.PrimaryConnectionString;
+        this.StaticEndpoint = storageAccount.PrimaryWebEndpoint;
     }
 
-    [Output]
-    public Output<string> ConnectionString { get; set; }
+    [Output] public Output<string> ConnectionString { get; set; }
+    [Output] public Output<string> StaticEndpoint { get; set; }
 }
